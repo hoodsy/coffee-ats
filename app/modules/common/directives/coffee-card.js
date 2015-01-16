f94e1ec1026204fcdcfc605ae25515a9af5db65c
jlas <juan.lasheras@gmail.com>
2015-01-16 17:34:24 -0500

    fix gulp script and jshint stuff

--- app/modules/common/directives/coffee-card.js
+++ app/modules/common/directives/coffee-card.js
@@ -4,7 +4,7 @@ angular.module('common')
   .directive('coffeeCard', function($compile, $interpolate) {
     return {
       restrict: 'A',
-      link: function(scope, element, attrs) {
+      link: function(scope, element) {
         var e = $interpolate('<div data-{{ item.type }}-coffee-card data-model="item"></div>')(scope);
         element.append($compile(e)(scope));
       }
