8d6ad0664c254b8cc50b833989c75ca98e44d99b
jlas <juan.lasheras@gmail.com>
2015-01-16 17:31:22 -0500

    Working on coffee cards

--- app/modules/opportunity/directives/opportunity-coffee-card.js
+++ app/modules/opportunity/directives/opportunity-coffee-card.js
@@ -2,11 +2,18 @@
 
 angular.module('opportunity')
   .directive('opportunityCoffeeCard', function() {
+
+    var count = 0;
+
     return {
       restrict: 'A',
       scope: {
         model: '='
       },
-      templateUrl: 'modules/opportunity/partials/opportunity-coffee-card.html'
+      templateUrl: 'modules/opportunity/partials/opportunity-coffee-card.html',
+      link: function(scope) {
+        count = (count + 1) % 3;
+        scope.count = count;
+      }
     };
 });
--- app/modules/user/directives/user-coffee-card.js
+++ app/modules/user/directives/user-coffee-card.js
@@ -2,11 +2,18 @@
 
 angular.module('user')
   .directive('userCoffeeCard', function() {
+
+    var count = 0;
+
     return {
       restrict: 'A',
       scope: {
         model: '='
       },
-      templateUrl: 'modules/user/partials/user-coffee-card.html'
+      templateUrl: 'modules/user/partials/user-coffee-card.html',
+      link: function(scope) {
+        count = (count + 1) % 5;
+        scope.count = count;
+      }
     };
 });
