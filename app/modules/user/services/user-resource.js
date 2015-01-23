4f8487cf54e8dd6b559181bdcdcc5908bb484a0b
jlas <juan.lasheras@gmail.com>
2015-01-23 10:30:11 -0500

    Dont send user id in requests

--- app/modules/user/services/user-resource.js
+++ app/modules/user/services/user-resource.js
@@ -1,10 +1,23 @@
 'use strict';
 
 angular.module('user')
-  .factory('User', function ($resource) {
+  .factory('User', function ($http, $resource) {
+
+    function prependTransform(transform) {
+      var defaults = $http.defaults.transformRequest;
+      defaults = angular.isArray(defaults) ? defaults : [defaults];
+      defaults = defaults.slice();
+      defaults.unshift(transform);
+      return defaults;
+    }
+
     return $resource('/api/users/:id', { id: '@_id'}, {
       update: {
-        method: 'PUT'
+        method: 'PUT',
+        transformRequest: prependTransform(function(value) {
+          delete value._id;
+          return value;
+        })
       }
     });
   });
