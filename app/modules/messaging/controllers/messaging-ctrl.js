b9a89fef8f5b30e2c464c9b2786c3e5510d62201
jlas <juan.lasheras@gmail.com>
2015-01-26 18:29:54 -0500

    [messaging] rename variable, add match delete handler, use generic prompt modal

--- app/modules/messaging/controllers/messaging-ctrl.js
+++ app/modules/messaging/controllers/messaging-ctrl.js
@@ -3,7 +3,7 @@
 angular.module('messaging')
   .controller('MessagingCtrl', function ($scope, $modal, UserMatch, Incident) {
 
-    $scope.detailShow = -1;
+    $scope.canShowControls = -1;
 
     UserMatch.query(function(response) {
       $scope.matches = response;
@@ -28,27 +28,53 @@ angular.module('messaging')
 
 
     $scope.toggleControls = function(index) {
-      if ($scope.detailShow === index) {
-        $scope.detailShow = -1;
+      if ($scope.canShowControls === index) {
+        $scope.canShowControls = -1;
       } else {
-        $scope.detailShow = index;
+        $scope.canShowControls = index;
       }
     };
 
     $scope.showControls = function(index) {
-      return ($scope.detailShow === index);
+      return ($scope.canShowControls === index);
     };
 
     // Launch Report model
     $scope.report = function(userId) {
+      var scope = $scope.$new();
+      scope.message = 'Report?';
+
       var modalInstance = $modal.open({
-        templateUrl: 'modules/messaging/partials/report-modal.html',
+        templateUrl: 'modules/common/partials/prompt-modal.html',
+        scope: scope,
         size: 'sm'
       });
 
-      // Handle affirmative
+      // Handle affirmative response
       modalInstance.result.then(function() {
         Incident.save({ userId: userId }, function() {
+          modalInstance.dismiss();
+        }, function() {
+          console.log('Implement me');
+          modalInstance.dismiss();
+        });
+      });
+    };
+
+    // Launch Delete model
+    $scope.delete = function(matchId) {
+      var scope = $scope.$new();
+      scope.message = 'Delete?';
+
+      var modalInstance = $modal.open({
+        templateUrl: 'modules/common/partials/prompt-modal.html',
+        scope: scope,
+        size: 'sm'
+      });
+
+      // Handle affirmative response
+      modalInstance.result.then(function() {
+        UserMatch.delete({ id: matchId }, function() {
           modalInstance.dismiss();
         }, function() {
           console.log('Implement me');
