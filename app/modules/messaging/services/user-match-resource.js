f2778065eed79410b770abdf62a37ef2e89ef014
jlas <juan.lasheras@gmail.com>
2015-01-30 18:16:38 -0500

    Fix resource definition for user matches

--- app/modules/messaging/services/user-match-resource.js
+++ app/modules/messaging/services/user-match-resource.js
@@ -2,9 +2,10 @@
 
 angular.module('messaging')
   .factory('UserMatch', function ($rootScope, $resource) {
-    return $resource('/api/users/:userId/matches/:id', { 'id': '@_id' }, {
-        userId: function() {
-          return $rootScope._user._id;
-        }
+    return $resource('/api/users/:userId/matches/:id', {
+      id: '@_id',
+      userId: function() {
+        return $rootScope._user._id;
+      }
     });
   });
