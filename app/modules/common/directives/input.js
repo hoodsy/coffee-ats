582c592663fc849dc3e48dd9ef16fce0e9d080fd
jlas <juan.lasheras@gmail.com>
2015-02-06 18:40:39 -0500

    [user] more form validation

--- app/modules/common/directives/input.js
+++ app/modules/common/directives/input.js
@@ -2,16 +2,28 @@
 
 angular.module('common')
   .directive('coffeeInput', function($compile, $interpolate) {
+
+    function createEl(inputName, errorType, errorMsg) {
+      return ('<span class="coffee-input-helper" data-ng-if="editForm.' +
+        inputName + '.$error.' +  errorType + '">' + errorMsg + '</span>');
+    }
+
+    var errorTypes = [
+      { type: 'required', msg: 'Required' },
+      { type: 'maxlength', msg: 'Too Long' }
+    ];
+
     return {
       restrict: 'A',
       link: function(scope, element) {
         var name = element.attr('name');
         if (name) {
-          var required = $interpolate(
-            '<span class="coffee-input-helper" ' +
-            'data-ng-if="editForm.' + name +
-            '.$error.required">Required</span>')(scope);
-          ($compile(required)(scope)).insertAfter(element);
+          errorTypes.forEach(function(type) {
+            if (element.attr(type.type) !== undefined) {
+              var el = $interpolate(createEl(name, type.type, type.msg))(scope);
+              ($compile(el)(scope)).insertAfter(element);
+            }
+          });
         }
       }
     };
