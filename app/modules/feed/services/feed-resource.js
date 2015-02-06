a23766e40a2a15b9c99a52db1f36a6c83453a3c3
jlas <juan.lasheras@gmail.com>
2015-02-06 12:10:08 -0500

    [feed] fix api url

--- app/modules/feed/services/feed-resource.js
+++ app/modules/feed/services/feed-resource.js
@@ -2,7 +2,7 @@
 
 angular.module('feed')
   .factory('Feed', function ($rootScope, $resource) {
-    return $resource('/api/user/:userId/feed', {
+    return $resource('/api/users/:userId/feed', {
         userId: function() {
           return $rootScope._user._id;
         }
