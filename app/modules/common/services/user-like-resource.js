e93ec626abae96bada9365de9dec576aa5b1e4d8
jlas <juan.lasheras@gmail.com>
2015-02-10 16:06:01 -0500

    [likes] fix missing userid in request

--- app/modules/common/services/user-like-resource.js
+++ app/modules/common/services/user-like-resource.js
@@ -2,7 +2,7 @@
 
 angular.module('common')
   .factory('UserLike', function ($rootScope, $resource) {
-    return $resource('/api/users/:userId/likes', {}, {
+    return $resource('/api/users/:userId/likes', {
         userId: function() {
           return $rootScope._user._id;
         }
