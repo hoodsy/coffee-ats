8290f79714f272c9931cbcae342346fa486e184f
jlas <juan.lasheras@gmail.com>
2015-03-05 18:40:36 -0500

    [redesign] adapt messaging page to desktop, tweak message bubbles, use special view for mobile messaging screens

--- app/modules/messaging/routes/routes.js
+++ app/modules/messaging/routes/routes.js
@@ -5,18 +5,34 @@ var app = angular.module('messaging');
 app.config(function ($stateProvider) {
     $stateProvider
       .state('shell.messaging', {
-        url: '/messaging',
+        abstract: true,
         views: {
           'main@shell': {
-            templateUrl: 'modules/messaging/partials/messaging.html',
+            templateUrl: 'modules/messaging/partials/messaging.html'
+          }
+        }
+      })
+      .state('shell.messaging.main', {
+        url: '/messaging',
+        views: {
+          'main@shell.messaging': {
+            templateUrl: 'modules/messaging/partials/messaging-main.html',
+            controller: 'MessagingCtrl'
+          },
+          'xs@shell.messaging': {
+            templateUrl: 'modules/messaging/partials/messaging-main.html',
             controller: 'MessagingCtrl'
           }
         }
       })
-      .state('shell.messaging.detail', {
+      .state('shell.messaging.main.detail', {
         url: '/:id',
         views: {
-          'main@shell': {
+          'detail@shell.messaging': {
+            templateUrl: 'modules/messaging/partials/messaging-detail.html',
+            controller: 'MessagingDetailCtrl'
+          },
+          'xs@shell.messaging': {
             templateUrl: 'modules/messaging/partials/messaging-detail.html',
             controller: 'MessagingDetailCtrl'
           }
