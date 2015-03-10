3666a1350ea7afcb736b39ff5e0d5a413d92aba5
jlas <juan.lasheras@gmail.com>
2015-03-10 16:53:35 -0400

    [messaging] fix socket problem, fix well width

--- app/modules/common/index.js
+++ app/modules/common/index.js
@@ -4,5 +4,9 @@ angular.module('common', [
   'partials',
   'ui.router',
   'ngTouch',
-  'monospaced.elastic'  // auto-resizing textarea
-]);
+  'monospaced.elastic',  // auto-resizing textarea
+  'matchMedia'
+]).run(function ($rootScope, screenSize) {
+  $rootScope.isDesktop = screenSize.is('lg');
+  $rootScope.isMobile = screenSize.is('xs, sm, md');
+});
