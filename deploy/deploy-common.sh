90170062b11ea5aa0a217fc74a0063ae9f5b09ad
jlas <juan.lasheras@gmail.com>
2015-03-16 12:59:59 -0400

    [deploy] add deploy for docker

---
+++ deploy/deploy-common.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# clear the git repo—there shouldn't be any changes here
+git reset --hard HEAD
+# pull the latest changes.
+git pull origin master
+
+#install global deps
+npm install -g bower gulp
+
+# remove old installed deps
+rm -rf node_modules
+rm -rf app/bower_components
+
+# install deps. exit if it fails
+tmpdir=$(mktemp -d -t ats.XXXX)
+npm install --ignore-scripts --cache=$tmpdir
+rc=$?;
+rm -rf $tmpdir
+if [[ $rc != 0 ]]; then
+    echo 'npm install failed.';
+    exit $rc;
+fi
+
+#install bower deps
+bower install --allow-root
+rc=$?;
+if [[ $rc != 0 ]]; then
+    echo 'bower install failed.';
+    exit $rc;
+fi
+
+# override config on Docker
+echo "dockerhost: $DOCKERHOST"
+if [[ "$DOCKERHOST" ]]; then
+    echo "Overriding with DOCKERHOST"
+    sed -i "s/localhost/$DOCKERHOST/" config.json
+fi
+
+# build
+gulp build
+rc=$?;
+if [[ $rc != 0 ]]; then
+    echo 'gulp build failed.';
+    exit $rc;
+fi
--- deploy/docker-deploy.sh
+++ deploy/docker-deploy.sh
@@ -4,4 +4,4 @@ DEPLOY_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 bash $DEPLOY_DIR/deploy-common.sh
 
 # start the server
-npm start
+gulp serve:dist:proxy:nosync
