ad1ac7dda722f45ca1c608eb5384067e05f7a42c
jlas <juan.lasheras@gmail.com>
2015-04-03 11:39:49 -0400

    [user] update sidebar on user update

--- app/modules/user/controllers/user-detail-ctrl.js
+++ app/modules/user/controllers/user-detail-ctrl.js
@@ -3,7 +3,14 @@
 angular.module('user')
   .controller('UserDetailCtrl', function ($scope, $rootScope, $stateParams, authService, User) {
     $scope.palette = $stateParams.palette || '1';
-    $scope.user = User.get({ id: $stateParams.id });
+    User.get({ id: $stateParams.id }, function(response) {
+      $scope.user = response;
+      if ($rootScope._user._id === response._id) {
+        _.assign($rootScope._user, response);
+      }
+    }, function(err) {
+      console.log('ERROR', err);
+    });
 
     // Only logged in user may edit their own profile
     $scope.isEditable = function() {
