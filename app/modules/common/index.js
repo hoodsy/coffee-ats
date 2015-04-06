297931875db7c666440a5851e62e6930c5f79459
jlas <juan.lasheras@gmail.com>
2015-04-06 18:25:16 -0400

    Revert "prevent submitting form on enter"
    This reverts commit 91a03e2ec3ad3600e6ef2a7ee2c687bd9cad7107.

--- app/modules/common/index.js
+++ app/modules/common/index.js
@@ -9,12 +9,4 @@ angular.module('common', [
 ]).run(function ($rootScope, screenSize) {
   $rootScope.isDesktop = screenSize.is('sm, md, lg');
   $rootScope.isMobile = screenSize.is('xs');
-
-  // Prevent submitting form on Enter
-  $(window).keydown(function(event){
-    if(event.keyCode == 13) {
-      event.preventDefault();
-      return false;
-    }
-  });
 });
