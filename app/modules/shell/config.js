1d65f0698ec5f05f7edb2478b3ac728c17310286
jlas <juan.lasheras@gmail.com>
2015-04-06 13:24:43 -0400

    point to beta server

--- app/modules/shell/config.js
+++ app/modules/shell/config.js
@@ -2,7 +2,7 @@ angular.module("shell")
 
 .constant("SOCKETIO_URL", "http://api.beta.coffeetheapp.com:4000")
 
-.constant("API_ROOT_URL", "http://localhost:3000/")
+.constant("API_ROOT_URL", "http://api.beta.coffeetheapp.com:3000")
 
 .constant("DEFAULT_PAGE_SIZE", 15)
 
