5aef0b4488b881a0900ead3742fe27e628780a95
jlas <juan.lasheras@gmail.com>
2015-04-16 17:59:46 -0400

    [edit] add maxlength limit to input fields

--- app/modules/common/index.js
+++ app/modules/common/index.js
@@ -8,10 +8,17 @@ angular.module('common', [
   'hmTouchEvents',
   'monospaced.elastic',  // auto-resizing textarea
   'matchMedia'
-]).run(function ($rootScope, screenSize) {
+]).constant('APP_CONFIG', {
+  'inputLimit': 50,
+  'inputLimitLong': 500,
+  'inputLimitHeadline': 140
+}).run(function ($rootScope, screenSize, APP_CONFIG) {
   $rootScope.isDesktop = screenSize.is('sm, md, lg');
   $rootScope.isMobile = screenSize.is('xs');
 
   // HammerJS prevents user selection by default, so unset that
   Hammer.defaults.stop_browser_behavior.userSelect = '';
+
+  // Config variables for entire app
+  $rootScope.config = APP_CONFIG;
 });
